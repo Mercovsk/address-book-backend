b6834acc7ea03fa999aca68d8851f2670f98bca7
Mercovsk <marc.the.dev28@gmail.com>
2026-05-28 16:09:47 +0800

    feat: add patch endpoint for records

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from sqlmodel import select
 
 from database import get_session, init_db, Session
-from models import Record, RecordCreateUpdate
+from models import Record, RecordCreateUpdate, RecordPatch
 
 from contextlib import asynccontextmanager
 import logging
@@ -61,6 +61,26 @@ def update_record(record_data: RecordCreateUpdate, record_id: int, session: Sess
 
     try:
         update_data = record_data.model_dump() # return a dictionary representation of the model
+        record.sqlmodel_update(update_data) # update the fields from update_record into record
+        session.add(record)
+        session.commit()
+        session.refresh(record)
+        return record
+    except Exception as e:
+        logger.error(f"Failed to update record to database: {str(e)}", exc_info=True)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal database storage error")
+    
+@app.patch("/records/{record_id}", response_model=Record, status_code=status.HTTP_200_OK)
+def patch_record(record_data: RecordPatch, record_id: int, session: Session = Depends(get_session)):
+    record = session.get(Record, record_id)
+
+    if not record:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Record with ID {record_id} not found")
+    
+    logger.info(f"Beginning validation and parsing to update record {record.model_dump()} into payload {record_data.model_dump()}")
+
+    try:
+        update_data = record_data.model_dump(exclude_unset=True) # return a dictionary representation of the model
         record.sqlmodel_update(update_data) # update the fields from update_record into record
         session.add(record)
         session.commit()
