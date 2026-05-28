b107288faac1a80a01939a9e797a89e73bfd1334
Mercovsk <marc.the.dev28@gmail.com>
2026-05-28 16:04:46 +0800

    feat: Create separate models for validation of CreateUpdate and database table Record

--- models.py
+++ models.py
@@ -2,8 +2,13 @@ from sqlmodel import Field, SQLModel
 
 from decimal import Decimal
 
-class Record(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class RecordBase(SQLModel):
     name: str
     latitude: Decimal = Field(max_digits=9, decimal_places=6)
     longitude: Decimal = Field(max_digits=9, decimal_places=6)
+
+class RecordCreateUpdate(RecordBase):
+    pass
+
+class Record(RecordBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
