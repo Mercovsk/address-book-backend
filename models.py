0a63164d8b4aa24027907657615ae3bc454093a3
Mercovsk <marc.the.dev28@gmail.com>
2026-05-28 16:09:31 +0800

    feat: create new model RecordPatch

--- models.py
+++ models.py
@@ -10,5 +10,10 @@ class RecordBase(SQLModel):
 class RecordCreateUpdate(RecordBase):
     pass
 
+class RecordPatch(RecordBase):
+    name: str | None = None
+    latitude: Decimal | None = Field(default=None, max_digits=9, decimal_places=6)
+    longitude: Decimal | None = Field(default=None, max_digits=9, decimal_places=6)
+
 class Record(RecordBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
